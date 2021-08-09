8f0b7dddb41c2b30341f5005d349cb917e184116
iivanov21 <iivanov21@hotmail.com>
2021-08-09 09:58:07 +0000

    Fixed build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-
+#export the environment variables in the script
+if [ -f .env ]
+then
+  export $(cat .env | sed 's/#.*//g' | xargs)
+fi
+#Terminate if the script fails
 set -e
 
 #Build image
 docker-compose build --parallel
 
 #Push images
-#docker login -u $DOCKER_ID_USR -p $DOCKER_ID_PASSWORD
+docker login -u $DOCKER_ID_USR -p $DOCKER_ID_PASSWORD
+
 docker-compose push
