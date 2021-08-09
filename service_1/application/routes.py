862f5cd44f921bdbb8e1aa7342cf251bff92f0e8
iivanov21 <iivanov21@hotmail.com>
2021-08-09 11:39:26 +0000

    Final test version of service 1

--- service_1/application/routes.py
+++ service_1/application/routes.py
@@ -5,6 +5,7 @@ from flask import Flask, render_template, request
 from wtforms import SubmitField
 from application import app, db
 from application.classes import CustomWeapon, ShowWeapon
+from sqlalchemy import asc, desc
 import random
 
 def random_weapon():#Design for service 2
@@ -58,6 +59,10 @@ def index():
     error=""   
     
     show_form=ShowWeapon()
+    show_form_one=ShowWeapon()
+    show_form_two=ShowWeapon()
+    show_form_three=ShowWeapon()
+
     if request.method=="POST":
         #Service 2: Get the weapon
         weapon_choice=random_weapon()
@@ -77,7 +82,27 @@ def index():
         show_form.skin_name.data = skin
         show_form.condition.data = condition
         show_form.rarity.data = rarity
-    return render_template('showLayout.html',show_form=show_form,message=error)
+
+        weapons=db.session.query(CustomWeapon).order_by(CustomWeapon.id.desc())
+        if weapons.count() > 1 : 
+            show_form_one.weapon_name.data = weapons[1].weapon_name
+            show_form_one.weapon_type.data = weapons[1].weapon_type
+            show_form_one.skin_name.data = weapons[1].skin_name
+            show_form_one.condition.data = weapons[1].condition
+            show_form_one.rarity.data = weapons[1].rarity
+        if weapons.count() > 2 : 
+            show_form_two.weapon_name.data = weapons[2].weapon_name
+            show_form_two.weapon_type.data = weapons[2].weapon_type
+            show_form_two.skin_name.data = weapons[2].skin_name
+            show_form_two.condition.data = weapons[2].condition
+            show_form_two.rarity.data = weapons[2].rarity
+        if weapons.count() > 3 : 
+            show_form_three.weapon_name.data = weapons[3].weapon_name
+            show_form_three.weapon_type.data = weapons[3].weapon_type
+            show_form_three.skin_name.data = weapons[3].skin_name
+            show_form_three.condition.data = weapons[3].condition
+            show_form_three.rarity.data = weapons[3].rarity
+    return render_template('showLayout.html',show_form=show_form,show_form_one=show_form_one,show_form_two=show_form_two,show_form_three=show_form_three,message=error)
 
 
     
