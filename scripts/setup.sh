7ba1cdc5774f9c8688d43901bf25237ed1be0303
iivanov21 <iivanov21@hotmail.com>
2021-08-10 16:39:01 +0000

    Stage message

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,4 +1,4 @@
-sudo apt install python3 python3-pip python3-pytest
+apt install python3 python3-pip python3-pytest
 pip3 install -r service_1/requirements.txt -r service_2/requirements.txt -r service_3/requirements.txt -r service_4/requirements.txt
 bash scripts/docker.sh
 bash scripts/docker-compose.sh
