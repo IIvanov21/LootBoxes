cdac93684dac3ce2dfd2c90e2646801416c90ed4
iivanov21 <iivanov21@hotmail.com>
2021-08-11 15:20:22 +0000

    database

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp $WORKSPACE/docker-compose.yaml ansible@swarm-manager
-ssh -i ~/.ssh/ansible_id  ansible@swarm-manager  "docker stack deploy --compose-file docker-compose.yaml loot-boxes"
+ssh -i ~/.ssh/ansible_id  ansible@swarm-manager  "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml loot-boxes"
