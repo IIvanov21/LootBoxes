c7a39ca86fda509538d1d77344c68de2fcc2a30b
iivanov21 <iivanov21@hotmail.com>
2021-08-11 12:30:54 +0000

    Removed duplicate

--- scripts/build.sh
+++ scripts/build.sh
@@ -11,7 +11,7 @@ docker-compose -f $WORKSPACE/docker-compose.yaml build --parallel
 #Push images
 docker login -u $DOCKER_ID_USR -p $DOCKER_ID_PSW
 
-docker-compose -f $WORKSPACE push
+docker-compose -f $WORKSPACE/docker-compose.yaml push
 
 exit
 EOF
--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,7 +1,7 @@
 sudo su - jenkins <<EOF
 sudo apt install python3 python3-pip python3-pytest
-pip3 install -r ${WORKSPACE}/service_1/requirements.txt -r ${WORKSPACE}/service_2/requirements.txt -r ${WORKSPACE}/service_3/requirements.txt -r ${WORKSPACE}/service_4/requirements.txt
-bash ${WORKSPACE}/scripts/docker.sh
-bash ${WORKSPACE}/scripts/docker-compose.sh
+pip3 install -r $WORKSPACE/service_1/requirements.txt -r $WORKSPACE/service_2/requirements.txt -r $WORKSPACE/service_3/requirements.txt -r $WORKSPACE/service_4/requirements.txt
+bash $WORKSPACE/scripts/docker.sh
+bash $WORKSPACE/scripts/docker-compose.sh
 exit
 EOF
