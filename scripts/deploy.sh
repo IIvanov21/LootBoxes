dd58399ec1147e522a87bcc511db9159bb2fb982
iivanov21 <iivanov21@hotmail.com>
2021-08-11 13:33:01 +0000

    Fixes for deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-scp ~/LootBoxes/docker-compose.yaml ansible@swarm-manager
+scp $WORKSPACE/docker-compose.yaml ansible@swarm-manager
 ssh -i ~/.ssh/ansible_id  ansible@swarm-manager  "docker stack deploy --compose-file docker-compose.yaml loot-boxes"
