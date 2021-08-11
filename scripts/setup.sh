de2352e30fbd02f3442fc3f06918a4d1cfa27a5f
iivanov21 <iivanov21@hotmail.com>
2021-08-11 10:17:50 +0000

    Setup.sh

--- scripts/setup.sh
+++ scripts/setup.sh
@@ -1,6 +1,8 @@
 sudo su - jenkins <<EOF
+git c
 sudo apt install python3 python3-pip python3-pytest
-pip3 install -r service_1/requirements.txt -r service_2/requirements.txt -r service_3/requirements.txt -r service_4/requirements.txt
+pip3 install -r ${WORKSPACE}/LootBoxes/service_1/requirements.txt -r ${WORKSPACE}/LootBoxes/service_2/requirements.txt -r ${WORKSPACE}/LootBoxes/service_3/requirements.txt -r ${WORKSPACE}/LootBoxes/service_4/requirements.txt
 bash scripts/docker.sh
 bash scripts/docker-compose.sh
+exit
 EOF
