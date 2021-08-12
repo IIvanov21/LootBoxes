d686e15b19ed852cdbcdf27a420b1990b064e2ba
iivanov21 <iivanov21@hotmail.com>
2021-08-12 13:32:20 +0000

    Testing line edited

--- scripts/test.sh
+++ scripts/test.sh
@@ -1,2 +1,2 @@
 
-python3 -m pytest --cov --cov-config=.coveragerc --cov-report xml:coverage.xml --junitxml junit.xml
+python3 -m pytest --cov --cov-config=.coveragerc --cov--report term-missing --cov-report xml:coverage.xml --junitxml junit.xml
