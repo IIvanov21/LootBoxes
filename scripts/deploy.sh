1e1d13017c53af6554c27f1c96e99a22a7b01b85
Ubuntu <iivanov21@hotmail.com>
2021-09-06 13:00:45 +0000

    II:Adjust ansible

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp $WORKSPACE/docker-compose.yaml ansible@swarm-manager
-ssh -i ~/.ssh/ansible_id  ansible@swarm-manager  "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml loot-boxes"
+ssh -i ~/.ssh/id_rsa  jenkins@swarm-manager  "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml loot-boxes"
