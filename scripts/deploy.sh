86534aea836e14454cfcd60c5bf97a0903e4ea0f
Ubuntu <iivanov21@hotmail.com>
2021-09-06 14:18:40 +0000

    Deploy

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 scp $WORKSPACE/docker-compose.yaml jenkins@swarm-manager
-ssh -i ~/.ssh/id_rsa  jenkins@swarm-manager  "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file docker-compose.yaml loot-boxes"
+ssh -i ~/.ssh/id_rsa  jenkins@swarm-manager  "export DATABASE_URI=$DATABASE_URI && docker stack deploy --compose-file ~/LootBoxes/docker-compose.yaml loot-boxes"
